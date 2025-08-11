febda1b7299b974f070e74fd3f63a2e94f12f0d9
RyePAC <ryan.d.harkins@gmail.com>
2025-08-11 11:06:43 -0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Scent Protocol</h1>
+        <h2>Precision Fragrance Technology</h2>
+        <p>Engineer Your Own Signature Protocol</p>
+        <p>🚀 Successfully Deployed!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
